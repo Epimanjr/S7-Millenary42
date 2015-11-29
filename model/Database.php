c317d22d76689282033df8912970f9833d695054
antoine <antoine@Antoine-PC>
2015-11-29 01:41:36 +0100

    permet une connexion à la bdd Sybase sur le serveur de l'université

---
+++ model/Database.php
@@ -0,0 +1,48 @@
+<?php
+class Database {
+    
+    private static $db;
+    
+    /*
+     * Constructeur vide
+     */
+    public function __construct() {}
+    
+    /*
+     * Retourne un objet de type PDO
+     * représentant une connexion à la base
+     * décrite dans le fichier de configuration
+     * 'seadili-db.ini'
+     */
+    public static function connect() {
+        try {
+            
+            // Création d'un tableau contenant les informations
+            // contenue dans le fichier de configuration
+            $ini = parse_ini_file("m42-db.ini");
+            
+            // Création du dsn depuis les informations recueillies
+            $dsn= $ini['driver'] . ":host=" . $ini['host'] . ";dbname=" . $ini['dbname'];
+            
+            // Création de l'instance PDO
+            $db = new PDO($dsn, $ini['username'], $ini['password']);
+            
+            // Configuration de l'encodage
+            $db->exec("SET CHARACTER SET utf8");
+            
+            return $db;
+        } catch (PDOException $e) {
+            throw new Exception("Connection: $dsn ".$e->getMessage(). '<br/>');
+        }
+    }
+    
+    public static function getConnection() {
+        if (isset(self::$db)) {
+            return self::$db;
+        } else {
+            self::$db = self::connect();
+            return self::$db;
+        }
+    }
+    
+}
