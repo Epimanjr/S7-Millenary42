8f506c8fcdaeffc519717558f36374449ae532cd
maxime <maxime@maxime-LDLC-15>
2015-11-29 15:05:25 +0100

    ajoute test sur Appartement

--- model/Appartement.php
+++ model/Appartement.php
@@ -1,30 +1,5 @@
 <?php
 
-echo "Salut!<br>";
-
-include_once "Database.php";
-
-$db = Database::getConnection();
-
-//$db->exec("CREATE TABLE Test(numero NUMERIC(5),nom CHAR(20) NOT NULL,commentaire CHAR(255))");
-//$db->exec("INSERT INTO Test VALUES(1,'Test1','Aurais-je enfin reussi ?')");
-//$db->exec("INSERT INTO Test VALUES(2,'Test2','Il semble que oui...')");
-
-try {
-
-    $sql = 'SELECT * FROM Test';
-    foreach ($db->query($sql) as $row) {
-        print $row['numero'] . "\t";
-        print $row['nom'] . "\t";
-        print $row['commentaire'] . "\n";
-    }
-} catch (Exception $e) {
-    $trace = $e->getTrace();
-    echo "Erreur pendant findById: $trace";
-}
-
-echo "<br>coucou";
-
 //TODO ici : classe appartement
 class Appartement {
 
